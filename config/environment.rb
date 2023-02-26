f32853f47376a1dcde2ae759c86fab04890964a0
osallahstevie <osallahstevie@gmail.com>
2023-02-26 12:49:32 +0300

    completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,9 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+
+require 'pry'
+require_relative '../config/environment'
+
+binding.pry
+"pls"
